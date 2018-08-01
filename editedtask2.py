43d794d29f0cef403d9576fa52bf0cc27cc23c4e
Cynthia Rose <roseateya@gmail.com>
2018-08-01 16:47:20 +0300

    First commit

---
+++ editedtask2.py
@@ -0,0 +1,9 @@
+import random
+
+words = [word.rstrip('\n') for word in open('words.txt')]
+randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
+
+
+
+reversePhrase = randomPhrase[::-1]
+print(reversePhrase)
